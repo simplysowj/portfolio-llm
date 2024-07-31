6e92c214e1055d6dd711f6744365d7a8de662009
sowjanya <90475451+simplysowj@users.noreply.github.com>
2024-07-30 22:03:23 -0400

    Update 4_Resume.py

--- pages/4_Resume.py
+++ pages/4_Resume.py
@@ -6,7 +6,7 @@ def local_css(file_name):
     with open(file_name) as f:
         st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
         
-local_css("style/style.css/")
+#local_css("style/style.css/")
 
 
 
@@ -14,7 +14,15 @@ st.title("📝 Resume")
 
 pdf_path = "images/Sowjanya_AI_resume.pdf"
 
-
+# Provide a download button for the PDF
+with open(pdf_path, "rb") as pdf_file:
+    pdf_bytes = pdf_file.read()
+    st.download_button(
+        label="Download Resume as PDF",
+        data=pdf_bytes,
+        file_name="Sowjanya_Data_science_latest_resume.pdf",
+        mime="application/pdf"
+    )
 
 # Inform users about the download button
 st.markdown(

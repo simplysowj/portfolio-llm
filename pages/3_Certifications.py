03a79efa9c49339002c2ff291a92a616f791b707
sowjanya <90475451+simplysowj@users.noreply.github.com>
2024-07-30 21:57:12 -0400

    Update 3_Certifications.py

--- pages/3_Certifications.py
+++ pages/3_Certifications.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from PIL import Image
-from constant import *
+
 def local_css(file_name):
     with open(file_name) as f:
         st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)

797a973d38f24070d730d779a06460355de84596
sowjanya <90475451+simplysowj@users.noreply.github.com>
2024-07-30 22:03:56 -0400

    Update 4_Resume.py

--- pages/4_Resume.py
+++ pages/4_Resume.py
@@ -32,8 +32,4 @@ st.markdown(
     unsafe_allow_html=True
 )
 
-with open("images/Sowjanya_AI_resume.pdf","rb") as f:
-      base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-      pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="1000mm" height="1000mm" type="application/pdf"></iframe>'
-      st.markdown(pdf_display, unsafe_allow_html=True)
-  
+

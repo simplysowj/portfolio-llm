be937df33b8561a5f52ff42893561c722d9f52c4
sowjanya <90475451+simplysowj@users.noreply.github.com>
2024-11-17 02:13:16 -0500

    Update 4_Resume.py

--- pages/4_Resume.py
+++ pages/4_Resume.py
@@ -12,7 +12,7 @@ def local_css(file_name):
 
 st.title("📝 Resume")
 
-pdf_path = "images/Sowjanya_AI_resume.pdf"
+pdf_path = "images/sowjanya_genai resume (1).pdf"
 
 # Provide a download button for the PDF
 with open(pdf_path, "rb") as pdf_file:

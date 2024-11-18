9965fcd3983f1bc69420433b23f14ee9c2f9c762
sowjanya <90475451+simplysowj@users.noreply.github.com>
2024-11-18 00:03:52 -0500

    Update Personal_Bot.py

--- Personal_Bot.py
+++ Personal_Bot.py
@@ -94,89 +94,181 @@ navbar_links = [
 # Render the horizontal navbar
 st.markdown(horizontal_navbar(navbar_links), unsafe_allow_html=True)
 st.title("Resume Q&A Chatbot")
-st.markdown("## How to use\n"
-            "1. Enter your [OpenAI API key](https://platform.openai.com/account/api-keys) in the sidebar and hit enter🔑\n"  # noqa: E501
-            "2.If you dont have api key please click this link https://www.guvi.in/rag/d0dfa9d5-3d3f-4e7d-a5e9-dbf8e3c5f4bd "
-           )
+def is_valid_json(data):
+    try:
+        json.loads(data)
+        return True
+    except json.JSONDecodeError:
+        return False
 
-api_key = st.sidebar.text_input('Enter your OpenAI API Key and hit Enter', type="password")
-# Input fields for the OpenAI API key
-#api_key = st.text_input("Enter your OpenAI API Key", type="password")
 
+#if "mongodB_pass" in os.environ:
+   # mongodB_pass = os.getenv("mongodB_pass")
+#else: mongodB_pass = st.secrets["mongodb"]["mongodB_pass"]
+# Setting up a mongo_db connection to store conversations for deeper analysis
+#uri = f"mongodb+srv://simplysowj:{mongodB_pass}@cluster0.96b5s.mongodb.net/"
+#uri = "mongodb+srv://simplysowj:"+mongodB_pass+"@cluster0.96b5s.mongodb.net/?retryWrites=true&w=majority"
+
+#@st.cache_resource
+#def init_connection():
+ #   return MongoClient(uri, server_api=ServerApi('1'))
+#client = init_connection()
+
+
+#db = client['conversations_db']
+#conversations_collection = db['conversations']
+
+if "OPENAI_API_KEY" in os.environ:
+    openai_api_key = os.getenv("OPENAI_API_KEY")
+else: openai_api_key = st.secrets["openai"]["OPENAI_API_KEY"]
+
+
+if not openai_api_key:
+    st.error("OpenAI API key is missing. Please provide it in Streamlit secrets or input it manually.")
+# Check if the API key is successfully retrieved
+if openai_api_key:
+    st.success("OpenAI API key successfully loaded!")
+else:
+    st.error("Unable to load OpenAI API key. Please provide it.")
+
+#Creating Streamlit title and adding additional information about the bot
+st.title("Sowjanya's resumeGPT")
+with st.expander("⚠️Disclaimer"):
+    st.write("""This is a work in progress chatbot based on a large language model. It can answer questions about Sowjanya""")
+
+path = os.path.dirname(__file__)
+
+
+# Loading prompt to query openai
+prompt_template = path+"/templates/template.json"
+prompt = load_prompt(prompt_template)
+#prompt = template.format(input_parameter=user_input)
+
+# loading embedings
+faiss_index = path+"/faiss_index"
+
+# Loading CSV file
+data_source = path+"/data/about_me.csv"
+pdf_source = path+"/data/resume.pdf"
+
+# Function to store conversation
+def store_conversation(conversation_id, user_message, bot_message, answered):
+    timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    data = {
+        "conversation_id": conversation_id,
+        "timestamp": timestamp,
+        "user_message": user_message,
+        "bot_message": bot_message,
+        "answered": answered
+    }
+   # conversations_collection.insert_one(data)
+
+embeddings=OpenAIEmbeddings(openai_api_key=openai_api_key)
+
+#using FAISS as a vector DB
+if os.path.exists(faiss_index):
+        vectors = FAISS.load_local(faiss_index, embeddings,allow_dangerous_deserialization=True)
+else:
+    # Creating embeddings for the docs
+    if data_source:
+        # Load data from PDF and CSV sources
+        pdf_loader = PyPDFLoader(pdf_source)
+        pdf_data = pdf_loader.load_and_split()
+        print(pdf_data)
+        csv_loader = CSVLoader(file_path=data_source, encoding="utf-8")
+        #loader.
+        csv_data = csv_loader.load()
+        data = pdf_data + csv_data
+        vectors = FAISS.from_documents(data, embeddings)
+        vectors.save_local("faiss_index")
+
+retriever=vectors.as_retriever(search_type="similarity", search_kwargs={"k":6, "include_metadata":True, "score_threshold":0.6})
+#Creating langchain retreval chain 
+chain = ConversationalRetrievalChain.from_llm(llm = ChatOpenAI(temperature=0.0,model_name='gpt-3.5-turbo', openai_api_key=openai_api_key), 
+                                                retriever=retriever,return_source_documents=True,verbose=True,chain_type="stuff",
+                                                max_tokens_limit=4097, combine_docs_chain_kwargs={"prompt": prompt})
+
+
+def conversational_chat(query):
+    with st.spinner("Thinking..."):
+        # time.sleep(1)
+        # Be conversational and ask a follow up questions to keep the conversation going"
+        result = chain({"system": 
+        "You are a Art's ResumeGPT chatbot, a comprehensive, interactive resource for exploring Sowjanya  (Sowjanya) Bojja 's background, skills, and expertise. Be polite and provide answers based on the provided context only. Use only the provided data and not prior knowledge.", 
+                        "question": query, 
+                        "chat_history": st.session_state['history']})
+    
+    if (is_valid_json(result["answer"])):              
+        data = json.loads(result["answer"])
+    else:
+        data = json.loads('{"answered":"false", "response":"Hmm... Something is not right. I\'m experiencing technical difficulties. Try asking your question again or ask another question about Sowjanya\'s professional background and qualifications. Thank you for your understanding.", "questions":["What is Sowjanya\'s professional experience?","What projects has Sowjanya worked on?","What are Sowjanya\'s career goals?"]}')
+    # Access data fields
+    answered = data.get("answered")
+    response = data.get("response")
+    questions = data.get("questions")
+
+    full_response="--"
+
+    st.session_state['history'].append((query, response))
+    
+    if ('I am tuned to only answer questions' in response) or (response == ""):
+        full_response = """Unfortunately, I can't answer this question. My capabilities are limited to providing information about Sowjanya Bojja's professional background and qualifications. If you have other inquiries, I recommend reaching out to Sowjanya on [LinkedIn](https://www.linkedin.com/in/sowjanya-bojja/). I can answer questions like: \n - What is Sowjanya's educational background? \n - Can you list Sowjanya's professional experience? \n - What skills does Sowjanya possess? \n"""
+        store_conversation(st.session_state["uuid"], query, full_response, answered)
+        
+    else: 
+        markdown_list = ""
+        for item in questions:
+            markdown_list += f"- {item}\n"
+        full_response = response + "\n\n What else would you like to know about Sowjanya? You can ask me: \n" + markdown_list
+        store_conversation(st.session_state["uuid"], query, full_response, answered)
+    return(full_response)
+
+if "uuid" not in st.session_state:
+    st.session_state["uuid"] = str(uuid.uuid4())
+
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-3.5-turbo"
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+
+        welcome_message = """
+            Welcome! I'm **Sowjanya's ResumeGPT**, specialized in providing information about Sowjanya's professional background and qualifications. Feel free to ask me questions such as:
+
+            - What is Sowjanya's educational background?
+            - Can you outline Sowjanya's professional experience?
+            - What skills and expertise does Sowjanya bring to the table?
+
+            I'm here to assist you. What would you like to know?
+            """
+        message_placeholder.markdown(welcome_message)
+        
+
+if 'history' not in st.session_state:
+    st.session_state['history'] = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if prompt := st.chat_input("Ask me about Sowjanya"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        
+        user_input=prompt
+        st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        full_response = ""
+        full_response = conversational_chat(user_input)
+        message_placeholder.markdown(full_response)
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
 # Define the path to your local PDF file
 pdf_file_path = "images/Sowjanya_AI.pdf"  
 
-# Text input for the question
-question = st.text_input("Enter your question about the resume:")
-
-# Define the fixed chain type and other configurations
-chain_type = 'stuff'
-chunk_size = 1000  # Fixed chunk size
-
-def load_and_process_pdf(file_path):
-    """Load and process the PDF to create a vector store."""
-    # Load document from PDF
-    loader = PyPDFLoader(file_path)
-    documents = loader.load()
-
-    # Split the documents into chunks
-    text_splitter = CharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=0)
-    texts = text_splitter.split_documents(documents)
-
-    # Create embeddings and vectorstore
-    embeddings = OpenAIEmbeddings()
-    db = Chroma.from_documents(texts, embeddings)
-    
-    return db
-
-def get_qa_answer(query):
-    """Generate the answer to the query using the QA chain."""
-    # Ensure the API key is set
-    os.environ["OPENAI_API_KEY"] = api_key
-
-    # Process the PDF
-    db = load_and_process_pdf(pdf_file_path)
-
-    # Create retriever and QA chain
-    retriever = db.as_retriever(search_type="similarity", search_kwargs={"k": 2})  # Using k=2 as an example
-    qa_chain = RetrievalQA.from_chain_type(
-        llm=OpenAI(), chain_type=chain_type, retriever=retriever, return_source_documents=True
-    )
-
-    # Define the custom prompt
-    name = "Sowjanya"
-    pronoun = "her"
-    prompt = f"""
-    You are Buddy, an AI assistant dedicated to assisting {name} in her job search by providing recruiters with relevant and concise information. 
-    If you do not know the answer, politely admit it and let recruiters know how to contact {name} to get more information directly from {pronoun}. 
-    Don't put "Buddy" or a breakline in the front of your answer.
-    """
-
-    # Get the answer
-    try:
-        result = qa_chain({"query": query, "prompt": prompt})
-        
-        if "I don't know" in result.get("result", ""):
-
-            result["result"] = f"Unfortunately, I couldn't find specific information on that topic. For more details or further assistance, please feel free to contact {name} directly at [simplysowj@gmai.com](mailto:simplysowj@gmai.com)."
-        
-    except Exception as e:
-        result = {"result": f"An error occurred: {str(e)}"}
-
-    return result
-
-if st.button("Run"):
-    if api_key and question:
-        with st.spinner("Processing..."):
-            result = get_qa_answer(question)
-
-        # Display the results
-        st.write("**Answer:**")
-        st.write(result["result"])
-
-        
-    else:
-        st.error("Please provide APi key then enter the question.")
 with st.container():
     col1,col2 = st.columns([8,3])
    

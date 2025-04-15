d3de8b3d1d75fcac75459ae79e9d82a253b6fc9b
sowjanya <90475451+simplysowj@users.noreply.github.com>
2025-04-15 19:49:42 -0400

    Update Personal_Bot.py

--- Personal_Bot.py
+++ Personal_Bot.py
@@ -85,7 +85,7 @@ def horizontal_navbar(links):
 # Define links for the navbar
 navbar_links = [
     {"label": "Medium Article", "url": "https://medium.com/@simplysowj"},
-    {"label": "Sample Project","url":"https://blippy-souji.streamlit.app/"},
+    {"label": "Sample Project","url":"https://salmon-rock-0b662ee0f.6.azurestaticapps.net/"},
     {"label": "LinkedIn", "url": "https://www.linkedin.com/in/sowjanya-bojja/"},
     {"label": "GitHub", "url": "https://github.com/simplysowj"},
    
